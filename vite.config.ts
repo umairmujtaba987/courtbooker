bd2fdd55fdbf55498390a3df3b4f6b01d79bf650
umairmujtaba987 <umairmujtaba987@gmail.com>
2025-12-03 15:09:25 +0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,15 @@ export default defineConfig({
     react(),
     runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
+      process.env.REPL_ID !== undefined
       ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
+        await import("@replit/vite-plugin-cartographer").then((m) =>
+          m.cartographer(),
+        ),
+        await import("@replit/vite-plugin-dev-banner").then((m) =>
+          m.devBanner(),
+        ),
+      ]
       : []),
   ],
   resolve: {
@@ -31,8 +31,12 @@ export default defineConfig({
     },
   },
   root: path.resolve(__dirname, "client"),
+  // build: {
+  //   outDir: path.resolve(__dirname, "dist/public"),
+  //   emptyOutDir: true,
+  // },
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: "dist",
     emptyOutDir: true,
   },
   server: {
