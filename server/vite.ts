04df9b1b33761f936788ae3231239d23bf03f537
umairmujtaba987 <umairmujtaba987@gmail.com>
2025-12-03 15:21:27 +0500

    Revert "update"
    This reverts commit a023a572cc01e7ac415f86a30eb4c5b334d0a507.

--- server/vite.ts
+++ server/vite.ts
@@ -6,6 +6,9 @@ import fs from "fs";
 import path from "path";
 import { nanoid } from "nanoid";
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const viteLogger = createLogger();
 
 export async function setupVite(server: Server, app: Express) {
@@ -36,7 +39,7 @@ export async function setupVite(server: Server, app: Express) {
 
     try {
       const clientTemplate = path.resolve(
-        import.meta.dirname,
+        __dirname,  
         "..",
         "client",
         "index.html",
